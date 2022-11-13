e721d1248cb67a0a30791de3f7c7356201d2104f
vicentepf78 <vicentepf78.gmail.com>
2022-11-13 17:20:21 -0300

    ci - add git actions for every push

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,3 +1,8 @@
+function main() {
+    const result = somar(10,10);
+    console.log("Vicente : " + result);
+}
+
 function somar(a, b) {
     return a + b;
 }
@@ -14,4 +19,4 @@ function dividir(a, b) {
     return a / b;
 }
 
-module.exports = {somar, subtrair}
+module.exports = {somar, subtrair, main}
