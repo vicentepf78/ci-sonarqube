8a36a7ff33fe7b5cb171c55a6f50de3db5e6ced4
vicentepf78 <vicentepf78.gmail.com>
2022-11-13 16:27:19 -0300

    Criando projeto ci em node

---
+++ src/calculadora.test.js
@@ -0,0 +1,11 @@
+const calculadora = require('./calculadora')
+
+test('Aplicar soma em A + B', () => {
+    const result = calculadora.somar(10,10);
+    expect(result).toEqual(20); 
+})
+
+test('Aplicar subtração em A - B', () => {
+    const result = calculadora.subtrair(20,10);
+    expect(result).toEqual(10);
+})
